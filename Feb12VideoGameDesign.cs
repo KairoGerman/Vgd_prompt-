d5f3a4a6dff02fdef77c02cbe69d3a3676063667
KairoGerman <kairog618@stemcollegiate.ca>
2025-02-12 09:09:07 -0700

    Update and rename Feb 12 Video Game to Feb12VideoGameDesign.cs

---
+++ Feb12VideoGameDesign.cs
@@ -0,0 +1,8 @@
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
